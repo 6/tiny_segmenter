d2ed4d243fb903cd1f573cfa9a174d97289d4747
Peter Graham <peterghm@gmail.com>
2013-03-30 22:30:47 -0700

    Improve specs

--- spec/tiny_segmenter_spec.rb
+++ spec/tiny_segmenter_spec.rb
@@ -4,20 +4,22 @@ require 'spec_helper'
 describe TinySegmenter do
   subject{ TinySegmenter.new }
 
-  it "tokenizes Japanese text fairly accurately" do
-    subject.segment("極めてコンパクトな日本語分かち書きソフトウェアです。").should == \
-      ["極めて", "コンパクト", "な", "日本", "語分", "かち", "書き", "ソフトウェア", "です", "。"]
-  end
+  describe "#segment" do
+    it "tokenizes Japanese text fairly accurately" do
+      subject.segment("極めてコンパクトな日本語分かち書きソフトウェアです。").should == \
+        ["極めて", "コンパクト", "な", "日本", "語分", "かち", "書き", "ソフトウェア", "です", "。"]
+    end
 
-  it "removes any whitespace-only or empty tokens" do
-    subject.segment("書かれた 極めて    コンパクト").should_not include("", " ")
-  end
+    it "removes any whitespace-only or empty tokens" do
+      subject.segment("書かれた 極めて    コンパクト").should_not include("", " ", nil)
+    end
 
-  it "tokenizes interspersed non-Japanese words correctly" do
-    subject.segment("TinySegmenterはRubyだけで").should == ["TinySegmenter", "は", "Ruby", "だけ", "で"]
+    it "tokenizes interspersed non-Japanese words correctly" do
+      subject.segment("TinySegmenterはRubyだけで").should == ["TinySegmenter", "は", "Ruby", "だけ", "で"]
+    end
   end
 
   it "has a version" do
-    TinySegmenter::VERSION.should_not be_empty
+    TinySegmenter::VERSION.should be_kind_of(String)
   end
 end
